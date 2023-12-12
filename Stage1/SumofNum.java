2dfab6f166943b094aeef3cef43415486ea96dce
Pravinya <108561388+PravinyaBagde@users.noreply.github.com>
2023-12-12 16:12:40 +0530

    Add files via upload

---
+++ Stage1/SumofNum.java
@@ -0,0 +1,14 @@
+public class SumofNum {
+    public static void main(String[] args) {
+        int sum = 0;
+        int i =1;
+
+        do {
+            sum = sum+i;
+            i++;
+        }
+        while(i <= 20);
+            System.out.println("Sum = " + sum);
+        }
+    }
+
