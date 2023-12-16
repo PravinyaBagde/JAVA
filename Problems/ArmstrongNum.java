4d4fe1baf660d242c6248c6a3e05854c0153c665
Pravinya <108561388+PravinyaBagde@users.noreply.github.com>
2023-12-17 00:27:47 +0530

    Add files via upload

---
+++ Problems/ArmstrongNum.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class ArmstrongNum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number to check if it's an Armstrong number: ");
+        int number = scanner.nextInt();
+
+        if (isArmstrongNumber(number)) {
+            System.out.println(number + " is an Armstrong number.");
+        } else {
+            System.out.println(number + " is not an Armstrong number.");
+        }
+
+        scanner.close();
+    }
+
+    private static boolean isArmstrongNumber(int num) {
+        int originalNumber = num;
+        int numberOfDigits = String.valueOf(num).length();
+        int sum = 0;
+
+        while (num > 0) {
+            int digit = num % 10;
+            sum += Math.pow(digit, numberOfDigits);
+            num /= 10;
+        }
+
+        return sum == originalNumber;
+    }
+}
