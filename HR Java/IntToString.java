f2e5bcb3b47882aee5c16437040fa4a682b012d8
Pravinya <108561388+PravinyaBagde@users.noreply.github.com>
2023-12-22 02:28:09 +0530

    Add files via upload

---
+++ HR Java/IntToString.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class IntToString {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+
+        String s = Integer.toString(n);
+        System.out.print("Good job");
+    }
+}
