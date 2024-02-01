a86d609cce1c12f2c5682d70aca9c34e583a1e33
Pravinya <108561388+PravinyaBagde@users.noreply.github.com>
2024-02-01 20:26:36 +0530

    Add files via upload

---
+++ Stage1/SBhello.java
@@ -0,0 +1,10 @@
+public class SBhello {
+        public static void main(String[] args) {
+            StringBuilder sb = new StringBuilder("H");
+            sb.append("e");
+            sb.append("l");
+            sb.append("l");
+            sb.append("o");
+            System.out.println(sb);
+        }
+    }
