d60895a5d3d09ab2c961b848745cafb267ea646d
Pravinya <108561388+PravinyaBagde@users.noreply.github.com>
2024-02-01 22:35:29 +0530

    Add files via upload

---
+++ Stage1/SbstringRev.java
@@ -0,0 +1,10 @@
+public class SbstringRev {
+    public static void main(String[] args) {
+        String original = "Mahadev";
+        StringBuilder reversed = new StringBuilder(original).reverse();
+
+        System.out.println("Original : " + original);
+        System.out.println("Reversed is : " + reversed);
+    }
+}
+
