7f3571385002783029e0573ef591cc6abef82f19
Pravinya <108561388+PravinyaBagde@users.noreply.github.com>
2024-02-02 23:51:42 +0530

    Add files via upload

---
+++ Sorting/src/BubbleSort.java
@@ -0,0 +1,32 @@
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] arr = {12, 43, 33, 22, 56, 89};
+
+        bubbleSort(arr);
+
+        System.out.print("Sorted array:");
+        printArray(arr);
+    }
+
+    static void bubbleSort(int[] array) {
+        int n = array.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void printArray(int[] arr) {
+        for (int value : arr) {
+            System.out.print(value + " ");
+        }
+        System.out.println();
+    }
+}
