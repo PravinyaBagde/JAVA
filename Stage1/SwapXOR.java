71b8424b22e4a5f889f27f9b7b1b00b8df4c4431
Pravinya <108561388+PravinyaBagde@users.noreply.github.com>
2024-02-02 21:11:04 +0530

    Add files via upload

---
+++ Stage1/SwapXOR.java
@@ -0,0 +1,22 @@
+public class SwapXOR {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 7;
+        System.out.print("Before Swapping, a = ");
+        System.out.print(a);
+        System.out.print(" ");
+        System.out.print("b = ");
+        System.out.print(b);
+        System.out.println("");
+
+        a ^= b;
+        b ^= a;
+        a ^= b;
+
+        System.out.print("After Swapping, a = ");
+        System.out.print(a);
+        System.out.print(" ");
+        System.out.print("b = ");
+        System.out.print(b);
+    }
+}
